8946bfdfbc9e8f88704acb0685d8006c0a452f2a
Yanguk <yanguk@duck.com>
2025-03-15 14:34:34 +0900

    bug set

---
+++ packages/next-two/app/page.tsx
@@ -0,0 +1,10 @@
+import { Camera } from "lucide-react";
+
+/** Add your relevant code here for the issue to reproduce */
+export default function Home() {
+  return (
+    <h1>
+      hello world <Camera />
+    </h1>
+  );
+}
